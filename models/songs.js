1ce2fa7c1cd5bb9cb50f8cd80068754218bdae6b
Ben Rosenbach <ben@benrosenbach.com>
2016-07-26 14:52:23 -0400

    Filling out more of the Songs model.
    * Adding schema for the links sub-document, not just array of strings
    * Adding schema for tagsGroups sub-document, using external schema
    * Adding scripture field
    * Adding active flag
    * Adding legacyId for referencing old ID

--- models/songs.js
+++ models/songs.js
@@ -1,13 +1,24 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var linkSchema = new Schema({
+	name: String,
+	url: String
+});
+
+var tagGroupSchema = require('./tagGroup.schema');
+
 var songSchema = new Schema({
   title: String,
   author: String,
   lyrics: String,
   notes: String,
+  scripture: String,
   quarantined: Boolean,
-  links: [String]
+  active: Boolean,
+  legacyId: Number,
+  links: [linkSchema],
+  tagGroups: [tagGroupSchema]
 });
 
 var Song = mongoose.model('Song', songSchema);
